a74491da8fecaa77f9e9b388547619315b9fbca0
Yehuda Bortz <yehudabortz@icloud.com>
2023-06-26 11:49:00 -0400

    feat: add social links

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx'
 import './globals.css'
 import { Darker_Grotesque } from 'next/font/google'
 import Link from 'next/link'
+import Image from "next/image"
 
 
 const inter = Darker_Grotesque({ subsets: ['latin'] })
@@ -21,10 +22,20 @@ export default function RootLayout({
     <html lang="en" >
       <meta property="og:image" content="/yehuda_bortz_photo.jpg" />
       <body className={clsx(inter.className, "bg-neutral-200 text-black max-w-screen-2xl w-[90vw] m-auto")} >
-        <nav className="flex w-full py-8">
+        <nav className="flex w-full py-8 gap-8 justify-between">
           <Link href="/Yehuda_Bortz_Resume.pdf" download="Yehuda_Bortz_Resume.pdf" target="_blank" className="text-bg text-xl underline underline-offset-8 decoration-1">
             Resume
           </Link>
+          <div className="flex gap-4">
+            <Link href="https://github.com/yehudabortz" target="_blank">
+              <span className="sr-only">GitHub</span>
+              <Image alt="GitHub Icon" width={30} height={30} src="/github-icon.svg" />
+            </Link>
+            <Link href="https://www.linkedin.com/in/yehudabortz" target="_blank">
+              <span className="sr-only">LinkedIn</span>
+              <Image alt="LinkedIn Icon" width={30} height={30} src="/linkedin-icon.svg" />
+            </Link>
+          </div>
         </nav>
         {children}
         <footer className='min-h-[100px]'></footer>
